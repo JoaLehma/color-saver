6f862b20bb4cfcafed75928e72b645098dc8feb4
Joachim Lehmann <joa.lehmann@web.de>
2022-07-24 16:11:50 +0200

    Update Color-Picker-Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,32 @@
 import "./Form.css";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [inputValue, setInputValue] = useState("#dd0e72");
+
+  function handleInputValue(event) {
+    setInputValue(event.target.value);
+  }
+
   return (
-<form className="form" >
-  <label for="color">ColorPicker</label>
-    <input name="color" id="color" type="color" />
-    <hr/>
-    <label for="hex">Hex-Color-Code</label>
-    <input id="hex" type="text" />
-    <hr />
-    <button>Add</button>
-  </form>
+    <form className="form">
+      <label for="color">ColorPicker:</label>
+      <input
+        type="color"
+        id="color"
+        value={inputValue}
+        onChange={handleInputValue}
+      />
+      <hr />
+      <label for="hex">Hex-Farbcode:</label>
+      <input
+        id="hex"
+        type="text"
+        value={inputValue}
+        onChange={handleInputValue}
+      />
+      <hr />
+      <button>Add</button>
+    </form>
   );
-};
+}
